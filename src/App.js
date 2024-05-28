ae7d7a29cb76c5016e3f47cfe2acb196e738ccc7
pradeep-majji <pradeepmajji702@gmail.com>
2024-05-28 17:17:48 +0530

    5th commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import AuthProvider from './context/AuthProvider';
 import AuthProviderBuyer from './context/AuthProviderBuyer';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import { ProtectedRoute,ProtectedRouteBuyer } from './components/ProtectedRoute';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ProtectedRoute, ProtectedRouteBuyer } from './components/ProtectedRoute';
 import Home from './pages/Home';
 import BuyerFavourite from './pages/BuyerFavourite';
 import BuyerFilter from './pages/BuyerFilter';
@@ -14,24 +14,24 @@ import BuyersLiked from './pages/BuyersLiked';
 function App() {
   return (
     <AuthProvider>
-		<AuthProviderBuyer>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/register" element={<SignUp />} />
-					<Route path="/login" element={<Login />} />
-					<Route element={<ProtectedRoute />}>
-						<Route path="/" element={<Home />} />
-						<Route path="/buyersliked" element={<BuyersLiked />} />
-					</Route>
-					<Route element={<ProtectedRouteBuyer />}>
-						<Route path="/buyerhome" element={<BuyerHome />} />
-						<Route path="/buyerfilter" element={<BuyerFilter />} />
-						<Route path="/buyerfavourite" element={<BuyerFavourite />} />
-					</Route>			
-				</Routes>
-			</BrowserRouter>
-		</AuthProviderBuyer>
-	</AuthProvider>
+      <AuthProviderBuyer>
+        <BrowserRouter basename={process.env.PUBLIC_URL || '/'}>
+          <Routes>
+            <Route path="/register" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/buyersliked" element={<BuyersLiked />} />
+            </Route>
+            <Route element={<ProtectedRouteBuyer />}>
+              <Route path="/buyerhome" element={<BuyerHome />} />
+              <Route path="/buyerfilter" element={<BuyerFilter />} />
+              <Route path="/buyerfavourite" element={<BuyerFavourite />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProviderBuyer>
+    </AuthProvider>
   );
 }
 
